7f15235f3a424a71c7fe02b6322d7ec111a2e40a
Rudolph Gottesheim <r.gottesheim@loot.at>
2020-02-18 10:03:43 +0100

    Implement storybook

---
+++ packages/seatmaps/stories/Block.stories.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { FC, useState } from 'react';
+import { Area, Block, Row, Seat, Seatmap, SeatProps, SeatShape } from '../src';
+
+export default {
+    title: 'Block',
+    component: Block,
+};
+
+interface TenByTenProps {
+    seatProps?: Partial<SeatProps> | (() => Partial<SeatProps>);
+    active?: string[];
+}
+
+const TenByTen: FC<TenByTenProps> = ({seatProps = {}, active: defaultActive = []}) => {
+    const [active, setActive] = useState<string[]>(defaultActive);
+    const oneThroughTen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    const toggle = (rowName: string, seatName: string) => {
+        const combined = `${rowName}/${seatName}`;
+        const set = new Set(active);
+        set.has(combined) ? set.delete(combined) : set.add(combined);
+        setActive(Array.from(set));
+    }
+    return (
+        <>
+            <style type="text/css">{`
+                .seatmap {
+                    width: 400px;
+                    height: 400px;
+                }
+            `}</style>
+            <Seatmap className="seatmap">
+                <Area>
+                    <Block>
+                        {oneThroughTen.map((rowName, rowIndex) => (
+                            <Row
+                                key={rowIndex}
+                                leftLabel={rowName}
+                                rightLabel={rowName}
+                                y={rowIndex * 100}
+                            >
+                                {oneThroughTen.map((seatName, seatIndex) => (
+                                    <Seat
+                                        key={seatIndex}
+                                        name={seatName}
+                                        color="#ff9900"
+                                        x={seatIndex * 100}
+                                        onClick={() => toggle(rowName, seatName)}
+                                        active={active.indexOf(`${rowName}/${seatName}`) !== -1}
+                                        hideName={seatIndex !== 0 && seatIndex !== 9}
+                                        {...(typeof seatProps === 'function' ? seatProps() : seatProps)}
+                                    />
+                                ))}
+                            </Row>
+                        ))}
+                    </Block>
+                </Area>
+            </Seatmap>
+        </>
+    );
+};
+
+export const Square = () => (
+    <TenByTen seatProps={{shape: SeatShape.SQUARE}}/>
+);
+
+export const Circular = () => (
+    <TenByTen seatProps={{shape: SeatShape.CIRCLE}}/>
+);
+
+export const ActiveSquare = () => (
+    <TenByTen seatProps={{shape: SeatShape.SQUARE}} active={['3/6', '3/7', '3/8']}/>
+);
+
+export const ActiveCircular = () => (
+    <TenByTen seatProps={{shape: SeatShape.CIRCLE}} active={['3/6', '3/7', '3/8']}/>
+);
+
+export const Disabled = () => (
+    <TenByTen seatProps={{disabled: true}}/>
+);
---
+++ packages/seatmaps/stories/Volume.stories.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { FC, useState } from 'react';
+import { Area, Seatmap, Volume, VolumeProps } from '../src';
+
+export default {
+    title: 'Volume',
+    component: Volume,
+};
+
+interface WrappedProps {
+    volumeProps?: Partial<VolumeProps>;
+}
+
+const volumes: Array<[string, number, number]> = [
+    ['Table 16', 0, 0],
+    ['Table 17', 600, 0],
+    ['Table 18', 0, 400],
+    ['Table 19', 600, 400],
+    ['Table 20', 0, 800],
+    ['Table 21', 600, 800],
+];
+
+const Wrapped: FC<WrappedProps> = ({volumeProps}) => {
+    const [active, setActive] = useState<{ [volumeLabel: string]: boolean }>({});
+    return (
+        <>
+            <style type="text/css">{`
+                .seatmap {
+                    width: 400px;
+                    height: 400px;
+                }
+            `}</style>
+            <Seatmap className="seatmap">
+                <Area>
+                    {volumes.map(([label, x, y], index) => (
+                        <Volume
+                            key={index}
+                            label={label}
+                            x={x}
+                            y={y}
+                            onClick={() => setActive({...active, [label]: !active[label]})}
+                            width={500}
+                            height={300}
+                            color="#ff9900"
+                            active={active[label]}
+                            {...volumeProps}
+                        />
+                    ))}
+                </Area>
+            </Seatmap>
+        </>
+    );
+};
+
+export const Rectangle = () => <Wrapped volumeProps={{shape: 'rectangle'}}/>;
+
+export const Ellipse = () => <Wrapped volumeProps={{shape: 'ellipse'}}/>;
+
+export const Rotated = () => <Wrapped volumeProps={{angle: 20}}/>;
+
+export const Disabled = () => <Wrapped volumeProps={{disabled: true}}/>;
