3f26387ba4a84d58d0cc8ee3fa33f1305e1204c3
Melanie <codingCookie@users.noreply.github.com>
2023-12-12 10:16:40 +0100

    adjust badge params (#16)
    * adjust badgeCount params
    * update example pic

--- packages/seatmaps/src/SeatCountBadge.tsx
+++ packages/seatmaps/src/SeatCountBadge.tsx
@@ -8,8 +8,8 @@ export interface SeatCountBadgeProps {
 }
 
 // to place the badge nicely on the inner right side of the volume
-const HORIZONTAL_BADGE_PADDING = 7;
-const VERTICAL_BADGE_PADDING = 3;
+const HORIZONTAL_BADGE_PADDING = 6;
+const VERTICAL_BADGE_PADDING = 2;
 
 export const SeatCountBadge = ({ containerWidth, count = 0, color = 'inherit' }: SeatCountBadgeProps) => {
     const x = l(containerWidth) - HORIZONTAL_BADGE_PADDING;
