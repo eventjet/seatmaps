fc94e75d7dfb6da53b89e00881e476bd64d6db83
codingCookie <github@menat.de>
2023-12-13 20:21:52 +0100

    eventjet/eventjet-2014#1402 adjust seatCountBadge layout + position

--- packages/seatmaps/src/SeatCountBadge.tsx
+++ packages/seatmaps/src/SeatCountBadge.tsx
@@ -7,12 +7,16 @@ export interface SeatCountBadgeProps {
     color?: string;
 }
 
+// to place the badge on the right side of the volume
+const HORIZONTAL_BADGE_PADDING = 2;
+const VERTICAL_BADGE_PADDING = 2;
+
 export const SeatCountBadge = ({ containerWidth, count = 0, color = 'inherit' }: SeatCountBadgeProps) => {
-    const x = l(containerWidth);
+    const x = l(containerWidth) - HORIZONTAL_BADGE_PADDING;
     return (
         <Badge
             x={x}
-            y={0}
+            y={VERTICAL_BADGE_PADDING}
             color={color}
             count={count}
         />
